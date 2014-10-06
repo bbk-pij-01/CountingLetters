72eadda1cb6fe0d69169fe92348f002e9e9505fd
igalna <igalna@gmail.com>
2014-10-06 15:43:12 +0100

    First Commit of Letter Counting program

---
+++ CountingLetters.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountingLetters {
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        String userString;
+        int count = 0;
+        
+        System.out.print("Enter some text > ");
+        userString = keyboard.nextLine();
+        
+        for (int index = 0; index < userString.length(); index++) {
+            if (userString.charAt(index) == 'e' || userString.charAt(index) == 'E') {
+                count++;
+            }
+        }
+        System.out.println("There were " + count + " letter \"E's\" in your text.");
+    }
+}
